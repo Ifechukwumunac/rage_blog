abcf0e9a4eaf7a7fcfec62c3c6fb63e790a7bc8f
munachiso <ifechukwumunachiso5@gmail.com>
2022-08-07 11:58:15 -0700

    bla

--- rage_blog/wsgi.py
+++ rage_blog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'black_blog_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rage_blog.settings')
 
 application = get_wsgi_application()
